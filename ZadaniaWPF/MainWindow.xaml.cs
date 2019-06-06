8cc0b328447446ce81a3d5bab6b141d764534a65
Marcin Zwiazek <zwiazekm@hotmail.com>
2019-06-06 15:10:57 +0200

    Koniec 4'go dnia.

--- ZadaniaWPF/MainWindow.xaml.cs
+++ ZadaniaWPF/MainWindow.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Szkolenie.ZadaniaLib;
+using System.IO;
+using System.Configuration;
+using Microsoft.Win32;
+using System.Xml.Linq;
 
 namespace ZadaniaWPF
 {
@@ -22,10 +26,21 @@ namespace ZadaniaWPF
     public partial class MainWindow : Window
     {
         OrganizerZadan zadania = new OrganizerZadan();
+        string localPath = ConfigurationManager.AppSettings["myPath"];
 
         public MainWindow()
         {
             InitializeComponent();
+            KontrolaFolderu();
+        }
+
+        public void KontrolaFolderu()
+        {
+            if (!Directory.Exists(localPath))
+            {
+                Directory.CreateDirectory(localPath);
+            }
+
         }
 
         private void NoweZadanie_Click(object sender, RoutedEventArgs e)
@@ -35,8 +50,86 @@ namespace ZadaniaWPF
             //Otwrcie okna
             if (dialog.ShowDialog().Value)
             {
+                var tytul = dialog.tytulText.Text;
+                DateTime termin = (dialog.terminDatePicker.SelectedDate.HasValue) ?
+                    dialog.terminDatePicker.SelectedDate.Value : DateTime.Today;
+                zadania.DodajZadanie(tytul, termin);
+                ZadaniGrid.ItemsSource = zadania.ListaZadan();
                 MessageBox.Show("Zadanie dodane", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show(zadania.ListaZadan().ToList().Count().ToString());
             }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ZadaniGrid.ItemsSource = zadania.ListaZadan();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            string plikZadan = "listazadan.txt";
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.DefaultExt = ".txt";
+            saveDialog.Filter = "Pliki tekstowe .txt | *.txt";
+            saveDialog.FileName = $"{localPath}{plikZadan}";
+
+            if (saveDialog.ShowDialog().Value)
+            {
+                var lista = zadania.ZadaniaNieZakonczone();
+                File.WriteAllLines(saveDialog.FileName, lista);
+                MessageBox.Show("Zapisane");
+            }
+        }
+
+        private void StreamButton_Click(object sender, RoutedEventArgs e)
+        {
+            var filePath = localPath + "listazadan.txt";
+            FileStream file = new FileStream(filePath, FileMode.Open);
+            BinaryReader br = new BinaryReader(file);
+            byte[] dane = new byte[br.BaseStream.Length];
+            int pozycja = 0;
+            int retByte;
+            while ((retByte=br.Read())!=-1)
+            {
+                dane[pozycja] = (byte)retByte;
+                pozycja += sizeof(byte);
+            }
+            br.Close();
+            file.Close();
+            string wynik = "";
+            foreach (var item in dane)
+            {
+                wynik += item;
+            }
+            MessageBox.Show(wynik);
+            file = new FileStream(filePath, FileMode.Open);
+            StreamReader sr = new StreamReader(file);
+            StringBuilder sb = new StringBuilder();
+            while (sr.Peek() != -1)
+            {
+                sb.AppendLine(sr.ReadLine());
+            }
+            sr.Close();
+            file.Close();
+
+            MessageBox.Show(sb.ToString());
+
+        }
+
+        private void LoadXml_Click(object sender, RoutedEventArgs e)
+        {
+            string file = localPath + "xmlzadanie.xml";
+            XDocument xdoc = XDocument.Load(file);
+            var wynik = from el in xdoc.Root.Elements()
+                        where el.Name == "zadanie"
+                        select el.Attribute("id").Value;
+            string info = "";
+            foreach (var item in wynik)
+            {
+                info += item;
+            }
+            MessageBox.Show(info);            
         }
     }
 }
