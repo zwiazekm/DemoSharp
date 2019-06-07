1ec8598c60102fafaa299c1c1dc31724ee704fe9
Marcin Zwiazek <zwiazekm@hotmail.com>
2019-06-07 12:00:55 +0200

    5 dzień 2'ga przerwa

--- DemoDBWPF/MainWindow.xaml.cs
+++ DemoDBWPF/MainWindow.xaml.cs
@@ -25,9 +25,13 @@ namespace DemoDBWPF
         //Connection string
         string cnnStr = @"Data Source=(localdb)\MSSQLLocalDB; " +
             "Initial Catalog=Zadania;Integrated Security=True";
+
+        ZadaniaDB dbz = new ZadaniaDB();
+
         public MainWindow()
         {
             InitializeComponent();
+            EfListBox.DataContext = dbz.Zadania;
         }
 
         private void AdoButton_Click(object sender, RoutedEventArgs e)
@@ -68,6 +72,59 @@ namespace DemoDBWPF
             MessageBox.Show( ds.Tables["Zadania"].Rows[1][1].ToString());
 
             
+        }
+
+        private void EFDemo_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in dbz.Zadania)
+            {
+                EfListBox.Items.Add(item.Tytul);
+            }
+        }
+
+        private void AddDataEF_Click(object sender, RoutedEventArgs e)
+        {
+            Zadanie noweZadanie = new Zadanie()
+            {
+                Tytul = "Test 1",
+                Termin = DateTime.Now.AddDays(1)
+            };
+
+            dbz.Zadania.Add(noweZadanie);
+
+            noweZadanie = new Zadanie()
+            {
+                Tytul = "Test 2",
+                Termin = DateTime.Now.AddDays(5)
+            };
+            dbz.Zadania.Add(noweZadanie);
+
+            dbz.SaveChanges();
+
+        }
+
+        private void SzukajButton_Click(object sender, RoutedEventArgs e)
+        {
+            var wynik = from z in dbz.Zadania
+                        where z.ZadanieID >= 2
+                        select z;
+
+            string lista = "";
+            foreach (var item in wynik)
+            {
+                lista += item + System.Environment.NewLine;
+            }
+            MessageBox.Show(lista);
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            var wynik = (from z in dbz.Zadania
+                        where z.ZadanieID ==3
+                        select z).Single();
+
+            wynik.Tytul = "Nowy tytul";
+            dbz.SaveChanges();
         }
     }
 }
---
+++ DemoDBWPF/Zadanie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DemoDBWPF
+{
+    public class Zadanie
+    {
+        public int ZadanieID { get; set; }
+
+        public string Tytul { get; set; }
+        public DateTime Termin { get; set; }
+
+        public virtual IEnumerable<Osoba> Osoby { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Tytul} - {Termin}";
+        }
+    }
+
+    public class Osoba
+    {
+        public int OsobaID { get; set; }
+        public string Nazwisko { get; set; }
+        public string Imie { get; set; }
+        public int Wiek { get; set; }
+
+        public virtual Zadanie Zadanie { get; set; }
+
+    }
+
+    public class ZadaniaDB: DbContext
+    {
+        public DbSet<Zadanie> Zadania { get; set; }
+        public DbSet<Osoba> Osoby { get; set; }
+    }
+}
